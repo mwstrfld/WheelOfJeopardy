bd8760abcdfe561f6c993662aa864ced1aa5cc18
jwessdabess <jpwess@gmail.com>
2017-07-19 17:37:52 -0700

    Initial QAF implementation

--- QuestionAndAnswerFacilitator.h
+++ QuestionAndAnswerFacilitator.h
@@ -18,5 +18,15 @@ public:
 protected:
     // Do for each category
     quint8 m_category1CurrentId;
-    QVector< QPair<QString, QString> > m_category1QuestionToAnserMap;
+    QVector< QPair<QString, QString> > m_category1QuestionToAnswerMap;
+    quint8 m_category2CurrentId;
+    QVector< QPair<QString, QString> > m_category2QuestionToAnswerMap;
+    quint8 m_category3CurrentId;
+    QVector< QPair<QString, QString> > m_category3QuestionToAnswerMap;
+    quint8 m_category4CurrentId;
+    QVector< QPair<QString, QString> > m_category4QuestionToAnswerMap;
+    quint8 m_category5CurrentId;
+    QVector< QPair<QString, QString> > m_category5QuestionToAnswerMap;
+    quint8 m_category6CurrentId;
+    QVector< QPair<QString, QString> > m_category6QuestionToAnswerMap;
 };
